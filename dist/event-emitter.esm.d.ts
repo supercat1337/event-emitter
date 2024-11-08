619a374881bfba13593dfb019b05fd110704fb7d
supercat <123203733+supercat911@users.noreply.github.com>
2024-11-08 13:44:04 +0300

    Update types and builds

--- dist/event-emitter.esm.d.ts
+++ dist/event-emitter.esm.d.ts
@@ -9,29 +9,29 @@ export class EventEmitter<T extends string> {
     };
     /**
      * on is used to add a callback function that's going to be executed when the event is triggered
-     * @param {T} event
+     * @param {T|"#has-listeners"|"#no-listeners"} event
      * @param {Function} listener
      * @returns {()=>void}
      */
-    on(event: T, listener: Function): () => void;
+    on(event: T | "#has-listeners" | "#no-listeners", listener: Function): () => void;
     /**
      * Remove an event listener from an event
-     * @param {T} event
+     * @param {T|"#has-listeners"|"#no-listeners"} event
      * @param {Function} listener
      */
-    removeListener(event: T, listener: Function): void;
+    removeListener(event: T | "#has-listeners" | "#no-listeners", listener: Function): void;
     /**
      * emit is used to trigger an event
-     * @param {T} event
+     * @param {T|"#has-listeners"|"#no-listeners"} event
      */
-    emit(event: T, ...args: any[]): void;
+    emit(event: T | "#has-listeners" | "#no-listeners", ...args: any[]): void;
     /**
      * Add a one-time listener
-     * @param {T} event
+     * @param {T|"#has-listeners"|"#no-listeners"} event
      * @param {Function} listener
      * @returns {()=>void}
      */
-    once(event: T, listener: Function): () => void;
+    once(event: T | "#has-listeners" | "#no-listeners", listener: Function): () => void;
     /**
      * Wait for an event to be emitted
      * @param {T} event
@@ -46,5 +46,26 @@ export class EventEmitter<T extends string> {
      * @returns {Promise<boolean>} - Resolves with true if any event was emitted, false if the time ran out.
      */
     waitForAnyEvent(events: T[], max_wait_ms?: number): Promise<boolean>;
+    /**
+     * Clear all events
+     */
+    clear(): void;
+    /**
+     * Clears all listeners for a specified event.
+     * @param {T|"#has-listeners"|"#no-listeners"} event - The event for which to clear all listeners.
+     */
+    clearEventListeners(event: T | "#has-listeners" | "#no-listeners"): void;
+    /**
+     * onHasEventListeners() is used to subscribe to the "#has-listeners" event. This event is emitted when the number of listeners for any event (except "#has-listeners" and "#no-listeners") goes from 0 to 1.
+     * @param {Function} callback
+     * @returns {()=>void}
+     */
+    onHasEventListeners(callback: Function): () => void;
+    /**
+     * onNoEventListeners() is used to subscribe to the "#no-listeners" event. This event is emitted when the number of listeners for any event (except "#has-listeners" and "#no-listeners") goes from 1 to 0.
+     * @param {Function} callback
+     * @returns {()=>void}
+     */
+    onNoEventListeners(callback: Function): () => void;
 }
 //# sourceMappingURL=event-emitter.esm.d.ts.map
